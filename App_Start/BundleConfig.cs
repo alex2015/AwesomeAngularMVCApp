b9531c973955a9d9a563859f4983d31abc75c4df
alex2015 <alex2015@rambler.ru>
2015-07-14 00:54:53 +0300

    14.07.2015

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,6 +10,10 @@ namespace AwesomeAngularMVCApp
         {
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/AwesomeAngularMVCApp.js"));
+
             BundleTable.EnableOptimizations = true;
         }
     }
